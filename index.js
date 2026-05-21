60bd32def92ac4e57aa2d814052cfc17334dd72a
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-05-21 12:21:41 +0600

    jwt is apply of update booking  data

--- index.js
+++ index.js
@@ -79,8 +79,8 @@ async function run() {
       res.send(result);
     });
 
-    //update booking
-    app.patch("/booking/:id",  async (req, res) => {
+    //Update Booking
+    app.patch("/booking/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = {
         _id: new ObjectId(id),
@@ -100,7 +100,7 @@ async function run() {
     });
 
     //delete booking
-    app.delete("/booking/:id",  async (req, res) => {
+    app.delete("/booking/:id", async (req, res) => {
       const id = req.params.id;
       const query = {
         _id: new ObjectId(id),
